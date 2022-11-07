0228274ff5705d0f2e446307f157a04a30b78a1a
TAR5899 <117653534+TAR5899@users.noreply.github.com>
2022-11-08 00:05:01 +0900

    Update BlueArchive1.py

--- BlueArchive1.py
+++ BlueArchive1.py
@@ -8,7 +8,7 @@ client = discord.Client(intents=intents)
 @client.event
 async def on_ready(): # 봇이 실행되면 한 번 실행됨
     print("이 문장은 Python의 내장 함수를 출력하는 터미널에서 실행됩니다\n지금 보이는 것 처럼 말이죠")
-    await client.change_presence(status=discord.Status.online, activity=discord.Game("학생회 업무 담당")
+    await client.change_presence(status=discord.Status.online, activity=discord.Game("학생회 업무 담당"))
 
 @client.event
 async def on_message(message):
